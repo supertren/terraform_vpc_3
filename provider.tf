49374ef1adce1d9d3a4f58f4011fce5afe843bb6
supertren1 <supertren@gmail.com>
2022-12-13 19:21:30 +0100

    change provider

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
   shared_config_files      = ["/c/Users/Miguel/.aws/config"]
   shared_credentials_files = ["/c/Users/Miguel/.aws/credentials"]
-  # profile = "mik"
+  profile = "mik"
  }
 
 provider "aws" { # required to avoid typing a region terraform-provider-aws issues/1043
